487ce2b9c02d1021b7ec0782170dceefb614f6f0
Tejveer Singh <130300113+tejveer7918@users.noreply.github.com>
2025-03-23 03:54:20 +0530

    Update app.py

--- dailytask/app.py
+++ dailytask/app.py
@@ -13,7 +13,7 @@ DATASET_FILE_ID = "1YF-GiDnT0jvDRvICM8xETegtmmnFJ3dw"  # Correct dataset file ID
 
 MODEL_PATH = "QR_Authentication_ResNet50.keras"
 DATASET_ZIP_PATH = "dataset.zip"
-DATASET_EXTRACT_PATH = "test"
+DATASET_EXTRACT_PATH = "dataset"
 
 # Download Model if not exists
 if not os.path.exists(MODEL_PATH):
